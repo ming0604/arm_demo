2c9bbf880396d017a19677e117a437ee988027c8
ming0604 <b0981340647@gmail.com>
2026-01-03 19:00:22 +0800

    Add speed parameter to launch file and update comments to English. Add time duration recording

--- src/demo_send_script_speed_pid_GUI_multi_file_move_to_init.cpp
+++ src/demo_send_script_speed_pid_GUI_multi_file_move_to_init.cpp
@@ -12,8 +12,8 @@
 #include <chrono>
 #include <vector>
 #include <cstdio>
-#include <iomanip> // 為了 std::setprecision
-#include <iostream> // 引入 iostream
+#include <iomanip> // For std::setprecision
+#include <iostream> // Include iostream
 
 std::fstream file;
 std::fstream file_start;
@@ -64,7 +64,7 @@ void TMmsgCallback(const tm_msgs::FeedbackState::ConstPtr& msg)
     ROS_ERROR_STREAM("Error FeedbackState callback (Pos)"); 
   }
 
-  // 讀取速度
+  // Read velocity
   if(msg->joint_vel.size() == 6)
   {
     for (int i = 0; i < 6; i++){
@@ -88,14 +88,18 @@ int main(int argc, char **argv)
   tm_msgs::SendScript srv;
 
   std::string csv_path;
+  std::string PTP_speed;
   // get parameters from launch file or command line
   nh_private.param<std::string>("traj_csv_path", csv_path, "/Default/path");
+  nh_private.param<std::string>("speed_percent", PTP_speed, "80");
   std::cout << "CSV Path: " << csv_path << std::endl;
+  std::cout << "PTP speed(%): " << PTP_speed << std::endl;
+
   std::vector<std::vector<float>> data;
 
   int count = 0;
   float speed_saturation = 20.0;
-  // 容許誤差 (度)
+  // Tolerance (deg)
   float tolerance = 0.5; 
   float init_joint[6];
  
@@ -108,15 +112,17 @@ int main(int argc, char **argv)
       return -1;
   }
 
-  // 建立回到初始點的 PTP 指令字串
+  // Create PTP command string to move to the initial point
   std::string cmd_init = "PTP(\"JPP\",";
+  std::string PTP_para = PTP_speed + ",100,0,false)";
 
   for (int i = 0; i < 6; i++){
     init_joint[i] = data[0][i];
     cmd_init += std::to_string(data[0][i]) + ",";
   }
   
-  cmd_init += "80,100,0,false)";
+  cmd_init += PTP_para;
+  //cmd_init += "80,100,0,false)";
   std::cout << "data size: " << data.size() << ", " << data[0].size() << std::endl;
   std::cout << "cmd_init: "<< cmd_init << std::endl;
 
@@ -126,7 +132,7 @@ int main(int argc, char **argv)
 
   ros::Rate rate(sampling_rate);
   
-  // 1. 先回到初始點 (程式啟動時)
+  // 1. Move to initial point first (when program starts)
   srv.request.id = "demo";
   srv.request.script = cmd_init;
   if (client.call(srv))                             
@@ -140,9 +146,10 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  auto start_time = std::chrono::steady_clock::now();
   ROS_INFO_STREAM("Move to initial position command sent. Waiting for robot to reach the position...");
   
-  // === 迴圈檢查是否到位 ===
+  // === Loop to check if position reached ===
   bool reached = false;
 
   while(ros::ok()) 
@@ -159,13 +166,13 @@ int main(int argc, char **argv)
         }
       }
 
-      // 顯示當前最大誤差
+      // Display current max error
       ROS_INFO_THROTTLE(2.0, "Current max position error: %.4f deg", max_error);
 
       if(max_error < tolerance) 
       {
         reached = true;
-        break; // 跳出迴圈
+        break; // Break loop
       }
 
       rate.sleep();
@@ -173,7 +180,10 @@ int main(int argc, char **argv)
 
   if(reached) 
   {
-    ROS_INFO("Robot has reached the initial position!");
+    auto end_time = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end_time - start_time;  // Defalt unit is seconds
+    ROS_INFO("Robot has reached the initial position within the angle tolerance %f.", tolerance);
+    ROS_INFO("Time taken: %f seconds", elapsed_seconds.count());
   }
 
   return 0;
